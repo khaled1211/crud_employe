9bdf7c4c67777f173cc1e2c06504da8b993ec1d3
khaled bouzid <khaled.bouzidi@esprit.tn>
2021-07-07 15:02:58 +0100

    ssssssssss200000000

---
+++ db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect('mongodb://localhost:27017/firstmongodb', (err) => {
+    if (!err)
+        console.log('MongoDB connection succeeded....');
+    else
+        console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
+});
+
+module.exports = mongoose;
